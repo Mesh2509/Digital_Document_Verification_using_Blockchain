07540fea33571a8a66e5951255ed04a855e2c20f
Prathamesh Apsingekar <pratham.ap25@gmail.com>
2024-07-14 21:01:18 +0530

    File Upload

--- main/frontend/src/mainComponents/Game.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Game = () => {
-  return <iframe height="700px" width="100%" src="./game/index.html" ></iframe>;
-};
-
-export default Game;
--- main/frontend/src/mainComponents/ToastSound.jsx
+++
@@ -1,6 +0,0 @@
-const playToastSound = () => {
-       const audio = new Audio('./music/toast_sound.mp3');
-     audio.play();
-  };
-  
-  export default playToastSound;
